1ef2fe81e50beaca00e6246458b701b08260c951
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-28 23:47:43 +0100

    user21con

--- src/components/Modals/ClaimModal.js
+++ src/components/Modals/ClaimModal.js
@@ -1,5 +1,6 @@
 import { GlobalContext } from "@/context/AppContext"
 import { useState } from "react"
+import Confetti from "react-confetti"
 //import { useGetUserId } from "@/hooks/useGetUserId"
 
 export const ClaimModal = () => {
@@ -11,6 +12,7 @@ export const ClaimModal = () => {
             <div className="h-[290px] ml-auto mr-auto py-2 px-2 w-[89%] bg-white/75  border-[#448cff]/90 border rounded-xl">
             {
                 claim ? <div className="mt-5 ml-auto mr-auto flex flex-col items-center justify-center text-center">
+                <Confetti className='w-[100%] h-[100%]' wind={true} /> 
                 <p className="text-center text-black font-light text-[20px] mb-6">{`${'Claim'}`} </p>
                 <div className="w-[80%] mb-2 ml-auto mr-auto py-1 px-3 flex  items-center justify-center rounded-full h-9">
                   <p className="text-black/85 text-[18px] font-light ml-auto mr-auto ">{'You Just Created a Solana Wallet'}</p>
