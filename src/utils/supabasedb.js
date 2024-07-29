9975b58859cd532ace94106113bde7d3f33e36df
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-29 05:27:57 +0100

    user21

--- src/utils/supabasedb.js
+++ src/utils/supabasedb.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const Url = 'https://hvzrbfjvsheuvxnrvpcv.supabase.co'
-const Key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2enJiZmp2c2hldXZ4bnJ2cGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3NjUyMDEsImV4cCI6MjAzNDM0MTIwMX0.gGyu7oZqojf2vvilHsNXZtAKotjO2OChAx6UTq61IjI'
+const Url = 'https://msdpxnahdkdwbqrinard.supabase.co'
+const Key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zZHB4bmFoZGtkd2JxcmluYXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyMTc0NDgsImV4cCI6MjAzNzc5MzQ0OH0.COj78fseoAIyZ2dG1lPAfGNtKXX5dYtUeIHDaF4UFkI'
 
 
 export const Supabase = createClient(
