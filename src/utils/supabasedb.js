eb2cbbdb5a82e78567130722c94cb806a04b4f81
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-29 04:38:14 +0100

    user2133

--- src/utils/supabasedb.js
+++ src/utils/supabasedb.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-require('dotenv').config() 
-const superUrl = process.env.SUPERURL;
-const superKey = process.env.SUPERKEY;
-export const supabase = createClient(superUrl,superKey)
+
+const Url = 'https://msdpxnahdkdwbqrinard.supabase.co'
+const Key = 'McEdLvO1l/GqIs4b3XfZqfpF4rWdx5bMeeAnW3evrIKXK2QJ5xXemJ/eMne1LcpzxOX5OHBHquka5chFMYU0nQ=='
+export const supabase = createClient(Url,Key)
