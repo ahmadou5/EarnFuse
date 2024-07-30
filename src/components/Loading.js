e142a35e748c445b335ad231c657d2510d121717
ahmadou5 <ahmadlasauwal@gmail.com>
2024-07-30 03:18:24 +0100

    user21we

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -35,7 +35,10 @@ export const Loading = () => {
     }
     useEffect(() => {
         alert('strenth')
-       createUser()
+        const interval = setInterval(() => {
+            alert('now ene')
+            createUser()
+         },2000)
     },[])
     return(
     <div className="inset-0 fixed bg-black/60 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
