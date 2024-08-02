2ae45c49d46145cbafe82a895cdeabf434f6b51a
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-02 01:45:35 +0100

    11166nnn

--- src/hooks/useGetBalance.js
+++ src/hooks/useGetBalance.js
@@ -29,6 +29,7 @@ export const UseGetBalance = () => {
                 console.log('hey balance data',data[0].balance)
                 console.log(data[0].Tasks,'aeki')
                 console.log(data[0].isClick,'counter')
+                console.log(data[0].refferals?.length(),'ref')
                
                 setUserBalance(data[0].balance)
               }
