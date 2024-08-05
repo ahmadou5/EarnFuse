e08236d7c4d64be20aa966411a60ca95ff28443e
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-05 23:38:29 +0100

    test

--- src/hooks/useGetBalance.js
+++ src/hooks/useGetBalance.js
@@ -5,6 +5,61 @@ export const UseGetBalance = () => {
     const { tgUser, setTgUser,userBalance,setUserBalance } = GlobalContext()
    
     useEffect(() => {
+      const test = async() => {
+          try {
+            const {data, error} = await SupabaseClient
+            .from('refferal')
+            .insert([{ refkey : `${refID}-${message.from.username}`, refId:`${refID}`, referId: `${message.from.id}` }])
+            .select('*')
+
+            if(data) {
+              console.log(data)
+            }
+            if(error) {
+              throw error
+            }
+          } catch (error) {
+            console.log(error)
+          }
+         
+      }
+      test()
+      async function inittest() {
+        if (
+          typeof window !== "undefined" &&
+          window.Telegram &&
+          window.Telegram.WebApp
+        ) {
+          console.log("Telegram WebApp is set");
+          const tgData = window.Telegram.WebApp;
+          console.log('data id',tgData?.initDataUnsafe?.user?.id)
+          
+         // setTgUser(tgData)
+          try {
+            console.log('Testingggg.....................')
+            const refID = tgData?.initDataUnsafe?.user?.id
+            const ref = tgData?.initDataUnsafe?.user?.username
+            const {data, error} = await SupabaseClient
+            .from('refferal')
+            .insert([{ refkey : `${refID}-${ref}`, refId:`${refID}`, referId: `${ref}` }])
+            .select('*')
+
+            if(data) {
+              console.log(data)
+            }
+            if(error) {
+              throw error
+            }
+          } catch (error) {
+            console.log(error,'testing error')
+          }
+        } else {
+          console.log("Telegram WebApp is undefined, retrying…");
+          //console.log(user);
+          setTimeout(initTg, 500);
+        }
+      }
+      inittest()
        async function initTg() {
             if (
               typeof window !== "undefined" &&
