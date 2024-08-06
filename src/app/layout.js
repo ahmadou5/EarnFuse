b66fe5c07b0a5937ef86834599b490c1dea51e55
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-06 06:24:29 +0100

    test2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { TelegramProvider } from "@/providers/TelegramProvider";
 import { TapContextProvider } from "@/context/AppContext";
 const inter = Montserrat({ subsets: ["latin"], weight: '400' });
 
@@ -13,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Script  src="https://telegram.org/js/telegram-web-app.js"></Script>
+      <TelegramProvider>
       <TapContextProvider>
        <body className={inter.className}>{children}</body>
       </TapContextProvider>
+      </TelegramProvider>
     </html>
   );
 }
