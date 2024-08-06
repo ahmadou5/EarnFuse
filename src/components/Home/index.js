cdbd7b64b9eeed5aa0b0da447595855d9476a944
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-06 06:02:45 +0100

    test2

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -90,7 +90,11 @@ export const Home2 = () => {
     const handleAnimationEnd = (id) => {
         setClicks((prevClick) => prevClick.filter(click => click.id !== id));
     }
-
+    function sendLink() {
+        WebApp.sendCopyRequest({
+          text: refLink // Replace with your desired link
+        });
+      }
     const refLink = `https://t.me/InFuseTapbot?start=${tgUser?.initDataUnsafe?.user?.id}`
    
     const updateBalance =  async () => {
@@ -342,7 +346,7 @@ export const Home2 = () => {
                     </div>
                 <div className="w-[100%] h-[150px] bg-blue-700/0 px-2 mt-8 p-4 flex">
                     <div className="w-[100%] p-4 flex h-[100%] text-white bg-white/0 rounded-xl">
-                     <div className="w-[45%] flex mr-auto items-center justify-center h-14  border-white/70 border-2 bg-black/0 rounded-3xl" >
+                     <div onClick={() => sendLink()} className="w-[45%] flex mr-auto items-center justify-center h-14  border-white/70 border-2 bg-black/0 rounded-3xl" >
                       <p className="text-[18px] font-bold">Invite Plug</p>
                      </div>
                      <div onClick={() => handleCopy(refLink)} className="w-[45%] ml-auto flex items-center justify-center h-14 border-2 border-white/70 bg-black/0 rounded-3xl" >
