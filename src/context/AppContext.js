a6179317e07c946b40a824b2ae538bbdf66a5c77
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-07 15:12:12 +0100

    test288

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -8,7 +8,7 @@ const TapContext = createContext()
 
 export const TapContextProvider = ({children}) => {
   const [reffs,setReff] = useState([])
-  const [lastClaim,setLastClaim] = useState()
+  const [lastClaim,setLastClaim] = useState('')
   const [userData,setUserData] = useState([])
   const [isConfe, setIsConfe] = useState(false)
   const [isAuth,setIsAuth] = useState(false)
@@ -45,6 +45,8 @@ export const TapContextProvider = ({children}) => {
     reffs,
     userData,
     isGame,
+    lastClaim,
+    setLastClaim,
     setIsGame,
     setUserData,
     setReff,
