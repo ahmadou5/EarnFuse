0eeacaa8ae1a69cd4eec4ff2b968c5d0bf3f45f6
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-08 19:43:55 +0100

    test288

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -128,7 +128,7 @@ export const Home2 = () => {
     const getTime = ({last}) => {
         const lastClaimData = new Date()
         const currentTime = new Date()
-        console.log('hello.',new Date())
+        console.log('hello.',new Date.now())
         //const twelveHoursInMs = 12 * 60 * 60 * 1000;
         const timeSinceInteraction = currentTime - lastClaimData;
 
