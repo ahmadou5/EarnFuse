01ac7dd6d2a36f4fe2098c9083e364c34938e623
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-09 11:59:23 +0100

    test288

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -412,7 +412,7 @@ export const Home2 = () => {
                     : 
                     <>
                     <div className="bg-black/30 w-[90%] rounded-2xl text-white flex items-center justify-center h-12">
-                        <div className="text-[18px]">
+                        <div className="text-[16px]">
                             {`Claim Available in ${formatTimeRemaining(timeRemaining)}`}
                         </div>
                     </div>
