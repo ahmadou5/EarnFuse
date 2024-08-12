54f69aa46159f6e7e126d98beeab6750d9d747fa
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-12 19:41:13 +0100

    dday22

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -389,7 +389,7 @@ export const Home2 = () => {
                     </div>
                     <div className="w-[100%] flex  text-white/75 items-center  flex-col justify-center mt-20">
                         <div>
-                        {`${item?.username[0]}${item?.username[Math.floor(item?.username.length / 2)]}`}
+                        {`${tgUser?.initDataUnsafe?.user?.username[0]}${tgUser?.initDataUnsafe?.user?.username[Math.floor(tgUser?.initDataUnsafe?.user?.length / 2)]}`}
                         </div>
                         <div>
                             <p className="font-bold text-[23px]">{`Hi ${tgUser?.initDataUnsafe?.user?.username}`}</p>
@@ -575,7 +575,7 @@ export const Home2 = () => {
                            className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-4 px-3 bg-black/15">
                             <div className="ml-1 mr-3">
                                 <div className="bg-black/50 rounded-full w-12 h-12">
-                                    {`${item?.username[0]} ${item?.username[Math.floor(item?.username.length / 2)]}`}
+                                    {`${userBoard?.username[0]}`}
                                 </div>
                             </div>
                             <div className="mt-1 text-sm flex ">
@@ -621,7 +621,7 @@ export const Home2 = () => {
                           key={i} className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-3 px-3 bg-white/15">
                             <div className="ml-1 mr-3">
                                 <div className="bg-black/50 rounded-full w-12 h-12">
-                                    {`${item?.username[0]} ${item?.username[Math.floor(item?.username.length / 2)]}`}
+                                    {`${item?.username[0]}`}
                                 </div>
                             </div>
                             <div className="mt-1 text-sm flex items-center justify-center">
