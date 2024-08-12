85234667e248fe4c0dcbe0ca3b14ba8bf24afe88
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-12 20:05:16 +0100

    dday22

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -580,9 +580,9 @@ export const Home2 = () => {
                             <div className="mt-1 w-[80%] text-sm flex ">
                                 <div className="ml-1 mr-auto">
                                     <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{userBoard?.username}</p>
-                                    <div className="mt-0.5 font-bold text-[13px] mb-0.5">{`${userBoard?.balance} $FUSE`}</div>
+                                    <div className="mt-0.5 font-bold text-white/40 text-[13px] mb-0.5">{`${userBoard?.balance?.toLocaleString()} FUSE`}</div>
                                 </div>
-                                <p className="mr-3 ml-auto font-bold mt-1 text-xl">{`#${userRank}`}</p>
+                                <p className="mr-3 ml-auto font-bold mt-1.5 text-xl">{`#${userRank}`}</p>
                             </div>
                             
                         </div>
@@ -610,25 +610,25 @@ export const Home2 = () => {
                     </div>
                 </div>**/}
                 <div className="w-[100%] h-auto px-2 mt-5 mb-2 py-5 flex flex-col justify-center items-center">
-                    <p>Booster</p>
+                    <p>{`${leads?.length} Fuse Earners`}</p>
                   
                     <div className="w-[100%] h-auto rounded-xl text-white/70 bg-black/0 p-0 mt-2">
                      {
                        leads && leads.map((item,i) => (
                         <>
                         <div 
-                          key={i} className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-3 px-3 bg-white/15">
-                            <div className="ml-1 mr-3">
-                                <div className="bg-black/50 rounded-full w-12 h-12">
+                          key={i} className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-3 px-3 bg-white/10">
+                            <div className="ml-1 mr-3 w-[20%]">
+                                <div className="bg-black/50 flex items-center justify-center rounded-full w-12 h-12">
                                     {`${item?.username[0]}`}
                                 </div>
                             </div>
-                            <div className="mt-1 text-sm flex items-center justify-center">
+                            <div className="mt-1 w-[80%] text-sm flex ">
                                 <div className="ml-2 mr-auto">
                                     <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{item?.username}</p>
-                                    <div className="mt-0.5 font-bold text-[13px] mb-0.5">{`${item?.balance} $FUSE`}</div>
+                                    <div className="mt-0.5 font-bold text-white/70 text-[13px] mb-0.5">{`${item?.balance?.toLocaleString()} FUSE`}</div>
                                 </div>
-                                <p className="mr-4 ml-auto font-bold text-xl">{`#${i + 1}`}</p>
+                                <p className="mr-3 ml-auto font-bold mt-1.5 text-xl">{`#${i + 1}`}</p>
                             </div>
                             
                         </div>
