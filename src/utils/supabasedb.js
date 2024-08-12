9af9afc338645a193474f9f1180ccfe513b3652d
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-12 14:31:00 +0100

    dday

--- src/utils/supabasedb.js
+++ src/utils/supabasedb.js
@@ -1,13 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from 'dotenv'
+import { dotenv } from 'dotenv'
+
 dotenv.config()
-
-
 
 
 const Url = process.env.SUPERURL
 const Key = process.env.SUPERKEY
-
+ 
+console.log('url',Url)
+console.log('key',Key)
 export const Supabase = createClient(
     Url,
     Key
