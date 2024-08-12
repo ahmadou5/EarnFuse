8912e9a1d021e2cd3be886589f1c9a724f829a92
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-12 19:38:04 +0100

    dday22

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -574,12 +574,14 @@ export const Home2 = () => {
                         <div 
                            className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-4 px-3 bg-black/15">
                             <div className="ml-1 mr-3">
-                                <img src="./assets/sol.png" className="w-12 h-12"/>
+                                <div className="bg-black/50 rounded-full w-12 h-12">
+                                    {`${item?.username[0]} ${item?.username[Math.floor(item?.username.length / 2)]}`}
+                                </div>
                             </div>
                             <div className="mt-1 text-sm flex ">
                                 <div className="ml-2 mr-auto">
-                                    <p className="mt-2 mb-2 font-bold text-[13px]">{userBoard?.username}</p>
-                                    <div className="mt-2 font-bold text-[13px mb-2">{`${userBoard?.balance} $FUSE`}</div>
+                                    <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{userBoard?.username}</p>
+                                    <div className="mt-0.5 font-bold text-[13px] mb-0.5">{`${userBoard?.balance} $FUSE`}</div>
                                 </div>
                                 <p className="mr-2 ml-auto font-bold text-xl">{`#${userRank}`}</p>
                             </div>
@@ -618,14 +620,14 @@ export const Home2 = () => {
                         <div 
                           key={i} className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-3 px-3 bg-white/15">
                             <div className="ml-1 mr-3">
-                                <div className="bg-black/50 w-12 h-12">
+                                <div className="bg-black/50 rounded-full w-12 h-12">
                                     {`${item?.username[0]} ${item?.username[Math.floor(item?.username.length / 2)]}`}
                                 </div>
                             </div>
                             <div className="mt-1 text-sm flex items-center justify-center">
                                 <div className="ml-2 mr-auto">
-                                    <p className="mt-2 mb-2 font-bold text-[13px]">{item?.username}</p>
-                                    <div className="mt-2 font-bold text-[13px mb-2">{`${item?.balance} $FUSE`}</div>
+                                    <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{item?.username}</p>
+                                    <div className="mt-0.5 font-bold text-[13px] mb-0.5">{`${item?.balance} $FUSE`}</div>
                                 </div>
                                 <p className="mr-4 ml-auto font-bold text-xl">{`#${i + 1}`}</p>
                             </div>
