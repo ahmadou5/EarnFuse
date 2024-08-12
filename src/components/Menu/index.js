1baa94b2c6e6c16e89bc838a0ace0c35d8db6794
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-12 14:56:34 +0100

    dday

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -13,7 +13,8 @@ import { GlobalContext } from "@/context/AppContext";
 import { RefferalSvg } from "../Svg/Refferal";
 import { HomeSvg } from "../Svg/HomeSvg";
 export const BackMenu = () => {
-  const { isHome,
+  const {
+    isHome,
     isFrens,
     isTask,
     isBoost,
@@ -21,133 +22,156 @@ export const BackMenu = () => {
     setIsTask,
     setIsGame,
     setIsFrens,
-    setIsHome
-  } = GlobalContext()
-  return(
-  <div
-    style={{ "backdrop-filter": "blur(12px)" }}
-    className=" w-[60px] h-[60px] ml-5 mr-auto rounded-full px-1.5 z-100 bg-white/15 mb-6 text-white/75  fixed inset-x-0 bottom-1 flex justify-center items-center"
-  >
-    <div className="lg:py-2.5 py-0 lg:px-2.5 px-1.5  mt-auto mb-auto ml-auto mr-auto w-[98%] flex items-center justify-center  h-[90%]">
-    <div onClick={() => {
-              setIsFrens(false)
-              setIsBoost(false)
-              setIsHome(true)
-              setIsTask(false)
-              setIsGame(false)
-            }} className={`h-11 ml-auto mr-auto w-[90%] bg-white/0 flex flex-col items-center justify-center`}>
-                 <img src="./assets/home.svg" className="w-8 h-8" />
-                
-            </div>
+    setIsHome,
+  } = GlobalContext();
+  return (
+    <div
+      style={{ "backdrop-filter": "blur(12px)" }}
+      className=" w-[60px] h-[60px] ml-5 mr-auto rounded-full px-1.5 z-100 bg-white/15 mb-6 text-white/75  fixed inset-x-0 bottom-1 flex justify-center items-center"
+    >
+      <div className="lg:py-2.5 py-0 lg:px-2.5 px-1.5  mt-auto mb-auto ml-auto mr-auto w-[98%] flex items-center justify-center  h-[90%]">
+        <div
+          onClick={() => {
+            setIsFrens(false);
+            setIsBoost(false);
+            setIsHome(true);
+            setIsTask(false);
+            setIsGame(false);
+          }}
+          className={`h-11 ml-auto mr-auto w-[90%] bg-white/0 flex flex-col items-center justify-center`}
+        >
+          <img src="./assets/home.svg" className="w-8 h-8" />
+        </div>
+      </div>
     </div>
-  </div>
-)
-}
-
+  );
+};
 
 export const Menu = () => {
-   const { isHome,
+  const {
+    isHome,
     isFrens,
     isTask,
     isBoost,
     setIsBoost,
     setIsTask,
     setIsFrens,
-    isGame,setIsGame,
-    setIsHome
-  } = GlobalContext()
-   const [isWallet,setIsWallet] = useState(true)
-   const [isSwap,setIsSwap] = useState(true)
-   const [isTokens,setIsTokens] = useState(true)
-   const [isHistory,setIsHistory] = useState(true)
-   
-    const router = useRouter()
-    const handleCopy = (value) => {
-      navigator.clipboard.writeText(value).then(
-        () => {
-          // Successfully copied to clipboard
-          setCopy(true);
-          setTimeout(  () => 
-            setCopy(false),
-            1000)
-          alert('address copied to clip Board')
-        },
-        (err) => {
-          // Failed to copy to clipboard
-          console.error('Could not copy address: ', err);
-        }
-      );
-    }
-    const handleCopy2 = (value) => {
-      navigator.clipboard.writeText(value).then(
-        () => {
-          // Successfully copied to clipboard
-          console.log('Address copied to clipboard');
-          alert('address copied to clip Board')
-          
-        },
-        (err) => {
-          // Failed to copy to clipboard
-          console.error('Could not copy address: ', err);
-        }
-      );
-    }
-    return (
-      <>
-        {/**for mobile view **/}
-        
-        {/**for desktop view **/}
-        <div
-          style={{ "backdrop-filter": "blur(12px)" }}
-          className=" w-[100%] ml-auto mr-auto rounded-xl py-1 px-1.5 z-100 bg-black/45  fixed bottom-1 flex justify-center items-center"
-        >
-          <div className="lg:py-2.5 py-1.5 lg:px-2.5 px-1.5  mt- mb-auto ml-auto mr-auto w-[98%] flex flex-row  h-[90%]">
-            <div onClick={() => {
-              setIsFrens(false)
-              setIsGame(false)
-              setIsBoost(false)
-              setIsHome(false)
-              setIsTask(true)
-            }} className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex flex-col items-center justify-center`}>
-                <img src="./assets/task.svg" className="w-7 h-7" />
-                <p className={`font-light mt-1 text-white text-[10px]`}>Tasks</p>
-            </div>
-            
-            <div onClick={() => {
-             setIsGame(true)
-             setIsFrens(false)
-             setIsBoost(false)
-             setIsHome(false)
-             setIsTask(false)
-            }} className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex flex-col items-center justify-center`}>
-                 <img src="./assets/pad.svg" className="w-7 h-7"/>
-                <p className={`font-light mt-1 text-white text-[10px]`}>Games</p>
-            </div>
-            <div onClick={() => {
-             setIsGame(false)
-             setIsFrens(false)
-             setIsBoost(true)
-             setIsHome(false)
-             setIsTask(false)
-            }} className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex flex-col items-center justify-center`}>
-                 <img src="./assets/board.svg" className="w-7 h-7"/>
-                <p className={`font-light mt-1 text-white text-[10px]`}>Leaderboard</p>
-            </div>
-            <div onClick={() => {
+    isGame,
+    setIsGame,
+    setIsHome,
+  } = GlobalContext();
+  const [isWallet, setIsWallet] = useState(true);
+  const [isSwap, setIsSwap] = useState(true);
+  const [isTokens, setIsTokens] = useState(true);
+  const [isHistory, setIsHistory] = useState(true);
 
-              setIsFrens(true)
-              setIsBoost(false)
-              setIsHome(false)
-              setIsTask(false)
-              setIsGame(false)
-            }}
-             className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex flex-col items-center justify-center`}>
-                 <img src="./assets/community.svg" className="w-7 h-7"/>
-                 <p className={`font-light mt-1 text-white text-[10px]`}>Plugs</p>
-            </div>
-            
-          </div>          
-        </div>
-      </>
+  const router = useRouter();
+  const handleCopy = (value) => {
+    navigator.clipboard.writeText(value).then(
+      () => {
+        // Successfully copied to clipboard
+        setCopy(true);
+        setTimeout(() => setCopy(false), 1000);
+        alert("address copied to clip Board");
+      },
+      (err) => {
+        // Failed to copy to clipboard
+        console.error("Could not copy address: ", err);
+      }
     );
   };
+  const handleCopy2 = (value) => {
+    navigator.clipboard.writeText(value).then(
+      () => {
+        // Successfully copied to clipboard
+        console.log("Address copied to clipboard");
+        alert("address copied to clip Board");
+      },
+      (err) => {
+        // Failed to copy to clipboard
+        console.error("Could not copy address: ", err);
+      }
+    );
+  };
+  return (
+    <>
+      {/**for mobile view **/}
+
+      {/**for desktop view **/}
+      <div
+        style={{ "backdrop-filter": "blur(12px)" }}
+        className=" w-[100%] ml-auto mr-auto rounded-xl py-1 px-1.5 z-100 bg-black/45  fixed bottom-1 flex justify-center items-center"
+      >
+        <div className="lg:py-2.5 py-1.5 lg:px-2.5 px-1.5  mt- mb-auto ml-auto mr-auto w-[98%] flex flex-row  h-[90%]">
+          <div
+            className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex items-center justify-center`}
+          >
+            <div
+              onClick={() => {
+                setIsFrens(false);
+                setIsGame(false);
+                setIsBoost(false);
+                setIsHome(false);
+                setIsTask(true);
+              }}
+            >
+              <img src="./assets/task.svg" className="w-7 h-7" />
+              <p className={`font-light mt-1 text-white text-[10px]`}>Tasks</p>
+            </div>
+          </div>
+
+          <div
+            className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex items-center justify-center`}
+          >
+            <div
+              onClick={() => {
+                setIsGame(true);
+                setIsFrens(false);
+                setIsBoost(false);
+                setIsHome(false);
+                setIsTask(false);
+              }}
+            >
+              <img src="./assets/pad.svg" className="w-7 h-7" />
+              <p className={`font-light mt-1 text-white text-[10px]`}>Games</p>
+            </div>
+          </div>
+          <div
+            className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex  items-center justify-center`}
+          >
+            <div
+              onClick={() => {
+                setIsGame(false);
+                setIsFrens(false);
+                setIsBoost(true);
+                setIsHome(false);
+                setIsTask(false);
+              }}
+            >
+              <img src="./assets/board.svg" className="w-7 h-7" />
+              <p className={`font-light mt-1 text-white text-[10px]`}>
+                Leaderboard
+              </p>
+            </div>
+          </div>
+          <div
+            className={`h-12 ml-auto mr-auto w-[25%] bg-white/0 flex  items-center justify-center`}
+          >
+            <div
+              onClick={() => {
+                setIsFrens(true);
+                setIsBoost(false);
+                setIsHome(false);
+                setIsTask(false);
+                setIsGame(false);
+              }}
+            >
+              <img src="./assets/community.svg" className="w-7 h-7" />
+              <p className={`font-light mt-1 text-white text-[10px]`}>Plugs</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
