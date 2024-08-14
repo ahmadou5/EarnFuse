41c6535d3564bb39657c65c7d2a27e22596ff301
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-14 15:30:25 +0100

    dday22

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -659,7 +659,7 @@ export const Home2 = () => {
                     </div>
                 </div>**/}
                 <div className="w-[100%] h-auto px-2 mt-5 mb-2 py-5 flex flex-col justify-center items-center">
-                    <p className="text-start text-xl font-bold">{`${leads?.length} Fuse Earners`}</p>
+                    <p className="text-start text-xl font-bold text-white/50">{`${leads?.length} Fuse Earners`}</p>
                   
                     <div className="w-[100%] h-auto rounded-xl text-white/70 bg-black/0 p-0 mt-2">
                      {
@@ -675,7 +675,7 @@ export const Home2 = () => {
                             <div className="mt-1 w-[85%] text-sm flex ">
                                 <div className="ml-2 mr-auto">
                                     <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{item?.username === null  ? 'User' : item?.username}</p>
-                                    <div className="mt-0.5 font-bold text-white/70 text-[13px] mb-0.5">{`${item?.balance?.toLocaleString()} FUSE`}</div>
+                                    <div className="mt-0.5 font-bold text-white/40 text-[13px] mb-0.5">{`${item?.balance?.toLocaleString()} FUSE`}</div>
                                 </div>
                                 <p className="mr-3 ml-auto font-bold mt-1.5 text-xl">{`#${i + 1}`}</p>
                             </div>
