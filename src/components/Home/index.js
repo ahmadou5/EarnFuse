55c03c60cda6845f0bd7590325c7a3aab83ae82b
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-14 15:14:07 +0100

    dday22

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -623,12 +623,12 @@ export const Home2 = () => {
                         <div className="w-[100%] mt-2 mb-2 h-auto flex rounded-xl py-4 px-3 bg-black/15">
                             <div className="ml-1 mr-3 w-[15%]">
                                 <div className="bg-black/50 flex items-center justify-center rounded-full w-12 h-12">
-                                    {`${userBoard?.username?.substring(0,1)}`}
+                                    {`${userBoard.username === undefined  ? 'U' : userBoard?.username?.substring(0,1)}`}
                                 </div>
                             </div>
                             <div className="mt-1 w-[85%] text-sm flex ">
                                 <div className="ml-1 mr-auto">
-                                    <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{userBoard?.username}</p>
+                                    <p className="mt-0.5 mb-0.5 font-bold text-[13px]">{userBoard.username === undefined ? 'User' : userBoard?.username}</p>
                                     <div className="mt-0.5 font-bold text-white/40 text-[13px] mb-0.5">{`${userBoard?.balance?.toLocaleString()} FUSE`}</div>
                                 </div>
                                 <p className="mr-3 ml-auto font-bold mt-1.5 text-xl">{`#${userRank}`}</p>
