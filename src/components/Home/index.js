fd2d9fe4342ac5313c111f690dd9acacfef5c9cc
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-15 12:23:32 +0100

    dday22

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -483,8 +483,8 @@ export const Home2 = () => {
                 ) : (
                   <>
                     <div className="bg-black/30 w-[90%] rounded-2xl text-white flex items-center justify-center h-[52px]">
-                      <div className="text-[16px] text-2xl text-blue-600">
-                      <div className="text-xl white">Claim Available in</div>
+                      <div className="text-[21px] flex text-blue-600">
+                      <div className="text-[16px] text-white">Claim Available in</div>
                         {`${formatTimeRemaining(
                           timeRemaining
                         )}`}
