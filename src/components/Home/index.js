6d6c0c5272651b678c6145040066a65b8f3480c6
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-17 14:54:55 +0100

    holaa

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -541,7 +541,7 @@ export const Home2 = () => {
                     <div
                       onClick={() => {
                         handleClaim()
-                        getTime()
+                        //getTime()
                         setCanClaim(false)
                         setClaimed(true)
                       }}
