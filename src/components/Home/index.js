fd364a8eeafdf00a0342d054675d62b799454368
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-17 16:00:43 +0100

    holaa

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -236,18 +236,106 @@ export const Home2 = () => {
         console.log(error)
        }
   }
+  const customHandle = async () => {
+    try {
+      if (
+        typeof window !== "undefined" &&
+        window.Telegram &&
+        window.Telegram.WebApp
+      ) {
+        console.log("Telegram WebApp is set");
+        const tgData = window.Telegram.WebApp;
+        console.log("data the first id", tgData?.initDataUnsafe?.user?.id);
+        const id = tgData?.initDataUnsafe?.user?.id.toString()
+
+        console.log( "tg user refferalss",id);
+
+         const { data, error } = await Supabase
+          .from("Users")
+          .select('*')
+          .order('balance',{ascending:false ,nullsFirst: false})
+
+          if (data) {
+            console.log('leaders',data)
+            setLeads(data)
+            console.log(id,'is it')
+            const filterone = data.find((item) => item.id === id )
+            setUserBoad(filterone)
+            const filterNumb = data.findIndex((item) => item.id === id )
+            setUserRank(filterNumb + 1)
+            console.log('user details', filterone)
+            console.log('user Rank', filterNumb + 1)
+            
+            console.log('filtered balance', filterone[0].balance)
+            //console.log(reffs,'it is')
+          }
+          if (error) {
+            //console.log("error", error);
+            throw error;
+          }
+       
+      } else {
+        //console.log("Telegram WebApp is undefined, retrying…");
+        //console.log(user);
+        setTimeout(initTg, 500);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const handleUpdateBoard = async () => {
     try {
+      const id = tgUser?.initDataUnsafe?.user?.id
       console.log('boarddddddddd')
       const { data, error } = await Supabase
           .from("Users")
           .select('*')
           .order('balance',{ascending:false ,nullsFirst: false})
 
-          if (data) {
+          if (data)if (
+            typeof window !== "undefined" &&
+            window.Telegram &&
+            window.Telegram.WebApp
+          ) {
+            console.log("Telegram WebApp is set");
+            const tgData = window.Telegram.WebApp;
+            console.log("data the first id", tgData?.initDataUnsafe?.user?.id);
+            const id = tgData?.initDataUnsafe?.user?.id.toString()
+    
+            console.log( "tg user refferalss",id);
+    
+             const { data, error } = await Supabase
+              .from("Users")
+              .select('*')
+              .order('balance',{ascending:false ,nullsFirst: false})
+    
+              if (data) {
+                console.log('leaders',data)
+                setLeads(data)
+                console.log(id,'is it')
+                const filterone = data.find((item) => item.id === id )
+                setUserBoad(filterone)
+                const filterNumb = data.findIndex((item) => item.id === id )
+                setUserRank(filterNumb + 1)
+                console.log('user details', filterone)
+                console.log('user Rank', filterNumb + 1)
+                
+                console.log('filtered balance', filterone[0].balance)
+                //console.log(reffs,'it is')
+              }
+              if (error) {
+                //console.log("error", error);
+                throw error;
+              }
+           
+          } else {
+            //console.log("Telegram WebApp is undefined, retrying…");
+            //console.log(user);
+            setTimeout(initTg, 500);
+          } {
             console.log('updated leaders',data)
             setLeads(data)
-            //console.log(id,'is it')
+            console.log(id,'is it')
             const filterone = data.find((item) => item.id === id )
             setUserBoad(filterone)
             const filterNumb = data.findIndex((item) => item.id === id )
