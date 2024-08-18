3bc6de18efef2775e7cc0ed9c359860be98cc2e0
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-18 04:11:52 +0100

    holaa

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -251,23 +251,13 @@ export const Home2 = () => {
         console.log( "tg user refferalss",id);
 
          const { data, error } = await Supabase
-          .from("users")
-          .select('*')
-          .order('balance',{ascending:false ,nullsFirst: false})
+           .rpc('get_unclaimed_tasks', {
+            userid : id
+         })
 
           if (data) {
-            console.log('leaders',data)
-            setLeads(data)
-            console.log(id,'is it')
-            const filterone = data.find((item) => item.id === id )
-            setUserBoad(filterone)
-            const filterNumb = data.findIndex((item) => item.id === id )
-            setUserRank(filterNumb + 1)
-            console.log('user details', filterone)
-            console.log('user Rank', filterNumb + 1)
+            console.log('wahala task',data)
             
-            console.log('filtered balance', filterone[0].balance)
-            //console.log(reffs,'it is')
           }
           if (error) {
             //console.log("error", error);
@@ -406,6 +396,7 @@ export const Home2 = () => {
 
   useEffect(() => {
     getTime();
+    customHandle()
   }, []);
 
   const todo = [
