2f344745b47d288d9c9f52109583ecf038617a3e
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-19 17:07:27 +0100

    holaa

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -197,15 +197,27 @@ export const Home2 = () => {
   };
 
   const handleUpdatedBalance = async () => {
+    console.log('balance updateee')
     try {
-      console.log(userData, "tg user");
-      const { data, error } = await Supabase.from("users")
-        .select("*")
-        .eq("id", tgUser?.initDataUnsafe?.user?.id);
+      if (
+        typeof window !== "undefined" &&
+        window.Telegram &&
+        window.Telegram.WebApp
+      ) {
+        console.log("Telegram WebApp is set");
+        const tgData = window.Telegram.WebApp;
+        console.log("data the first id", tgData?.initDataUnsafe?.user?.id);
+        const id = tgData?.initDataUnsafe?.user?.id.toString();
 
-      if (data) {
-        const sele = JSON.stringify(data);
-        console.log(sele, "cele ne");
+        console.log("task id", id);
+
+        const { data, error } = await Supabase.from("users")
+        .select("*")
+        .eq("id", tgData?.initDataUnsafe?.user?.id);
+
+        if (data) {
+       // const sele = JSON.stringify(data);
+        //console.log(sele, "cele ne");
         console.log("hey balance data", data[0].balance);
         console.log(data[0].id, "aeki");
         console.log(data[0].isClick, "counter");
@@ -213,14 +225,20 @@ export const Home2 = () => {
         setUserData(data);
         //setLastClaim(data[0].lastRewardClaim)
         //   setUserBalance(data[0].balance)
-      }
-      if (error) {
-        console.log("error", error);
-        throw error;
+        }
+        if (error) {
+          //console.log("error", error);
+          throw error;
+        }
+      } else {
+        //console.log("Telegram WebApp is undefined, retrying…");
+        //console.log(user);
+        setTimeout(initTg, 500);
       }
     } catch (error) {
       console.log(error);
     }
+   
   };
   const customHandleDone = async () => {
     try {
