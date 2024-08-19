7cd930b24e1cbcb916b6f3c9bf67dd1920ec524a
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-19 14:26:56 +0100

    holaa

--- src/utils/supabasedb.js
+++ src/utils/supabasedb.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const Url = 'https://msdpxnahdkdwbqrinard.supabase.co'
-const Key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zZHB4bmFoZGtkd2JxcmluYXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyMTc0NDgsImV4cCI6MjAzNzc5MzQ0OH0.COj78fseoAIyZ2dG1lPAfGNtKXX5dYtUeIHDaF4UFkI'
+const Url = process.env.NEXT_PUBLIC_SUPERURL
+const Key = process.env.NEXT_PUBLIC_SUPERKEY
 
 
 export const Supabase = createClient(
