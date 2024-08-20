4945f3c3fc944ac3108305494cc0a1f7a8253584
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-20 04:08:01 +0100

    holaa

--- src/components/Modals/ClaimModal.js
+++ src/components/Modals/ClaimModal.js
@@ -282,9 +282,7 @@ export const ClaimModal = () => {
             <div className="h-[340px] ml-auto mr-auto py-2 px-2 w-[95%] bg-white/75  border-[#448cff]/90 border rounded-xl">
             {
                 claim ? <div className="mt-5 ml-auto mr-auto flex flex-col items-center justify-center text-center">
-                <div className="w-[85px] h-[85px] mb-2 mt-1">
-                  <Image src={getTaskUrl(taskType)} className="w-[100%] h-[100%]"/>
-                </div>
+              
                 <p className="text-center text-black font-light text-[20px] mb-6">{`${'Claim'}`} </p>
                 <div className="w-[90%] mb-5 ml-auto mr-auto py-1 px-3 flex  items-center justify-center rounded-full h-9">
                   <p className="text-black/85 text-[18px] font-light ml-auto mr-auto ">{`Click on the Button to Claim ${taskAmount.toLocaleString()} Fuse Points`}</p>
@@ -308,11 +306,11 @@ export const ClaimModal = () => {
               </div> 
                 
             </div> : <div className="mt-5 ml-auto mr-auto flex flex-col items-center justify-center text-center">
-                <div className="w-[85px] h-[85px] mt-1 mb-2">
+                <div className="w-[85px] h-[85px] mt-1 mb-1">
                   <Image src={getTaskUrl(taskType)} className="w-[100%] h-[100%]"/>
                 </div>
-                <p className="text-center text-black font-light text-[29px] mb-10">{`${taskName}`} </p>
-                <div className="w-[90%] mb-5 ml-auto mr-auto py-1 px-3 flex  items-center justify-center rounded-full h-9">
+                <p className="text-center text-black font-light text-[29px] mb-7">{`${taskName}`} </p>
+                <div className="w-[90%] mb-3 ml-auto mr-auto py-1 px-3 flex  items-center justify-center rounded-full h-9">
                   <p className="text-black/85 text-[18px] font-light ml-auto mr-auto ">{`Complete task and earn ${taskAmount.toLocaleString()} Fuse Points`}</p>
                 </div>
                 <div onClick={() => {
@@ -320,7 +318,7 @@ export const ClaimModal = () => {
                     setTimeout(() => {
                         setClaim(true)
                       }, 2000);
-                    }} className="w-[175px] mt-10  ml-auto mr-auto py-1 px-3 text-white border  border-[#448cff]/60 flex  items-center justify-center bg-black/90 rounded-full h-9">
+                    }} className="w-[175px] mt-8  ml-auto mr-auto py-1 px-3 text-white border  border-[#448cff]/60 flex  items-center justify-center bg-black/90 rounded-full h-9">
                   <p>{taskButton}</p>
                 </div>
             </div>
