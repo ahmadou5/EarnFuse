e124365532691d0cc67a1fcf3e7fa08630f8fa93
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-20 01:02:52 +0100

    holaa

--- src/components/Modals/ClaimModal.js
+++ src/components/Modals/ClaimModal.js
@@ -256,11 +256,10 @@ export const ClaimModal = () => {
       taskClaimed()
       handleUpdatedBalance()
       handleUpdateBoard()
-    };
-    useEffect(() => {
       handleGetClaimedTasks()
       handleGetUnClaimedTasks()
-    },[handleClaimTask])
+    };
+    
 
     return(
     <div className="inset-0 fixed bg-white/0 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
