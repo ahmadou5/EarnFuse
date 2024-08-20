65914101e4d253cc1a0e64de5e1d9c7bab3b4c7f
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-20 04:38:08 +0100

    holaa

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -646,6 +646,9 @@ export const Home2 = () => {
                
               </div>
                     <div className="mt-5 ml-auto mr-auto flex flex-col items-center justify-center text-center">
+                      <div className="mt-6 mb-4 flex items-center justify-center">
+                        <p className="text-white font-bold text-3xl">Your daily rewards</p>
+                      </div>
                       <div className="w-[80%] mb-2 ml-auto mr-auto py-1 px-3 flex  items-center justify-center rounded-full mt-8 h-9">
                         <p className="text-black/85 text-[18px] font-light ml-auto mr-auto ">{`You Just Claim ${claimVal} as Your Daily Reward`}</p>
                       </div>
