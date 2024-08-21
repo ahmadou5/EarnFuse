16d2d67d74edebdbafd858a80d989e58e8fca57d
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-21 15:00:48 +0100

    holaa

--- src/components/Modals/ClaimModal.js
+++ src/components/Modals/ClaimModal.js
@@ -160,6 +160,48 @@ export const ClaimModal = () => {
         console.log(error);
       }
     }
+    const updateBalance = async() => {
+      if (
+        typeof window !== "undefined" &&
+        window.Telegram &&
+        window.Telegram.WebApp
+      ) {
+        console.log("Telegram WebApp is set");
+        const tgData = window.Telegram.WebApp;
+        console.log('data id',tgData?.initDataUnsafe?.user?.id)
+        
+        setTgUser(tgData)
+        try {
+        console.log(tgUser, 'tg user')
+        const { data, error } = await Supabase
+        .from('users')
+        .select('*')
+        .eq('id',tgData?.initDataUnsafe?.user?.id)
+
+        if(data) {
+        
+          console.log('hey balance data',data[0].balance)
+         // console.log(data[0].id,'aeki')
+         // console.log(data[0].isClick,'counter')
+         // console.log(data[0].lastClaim,'claim')
+        //  setUserData(data)
+        //  setLastClaim(data[0].lastRewardClaim)
+          setUserBalance(data[0].balance)
+        }
+        if(error) {
+          console.log('error',error)
+          throw error
+        }
+          
+         } catch (error) {
+          console.log(error)
+         }
+      } else {
+        console.log("Telegram WebApp is undefined, retrying…");
+        //console.log(user);
+        setTimeout(initTg, 500);
+      }
+    }
     const UpdateBalanceByTask = async () => {
       try {
         if (
@@ -271,6 +313,7 @@ export const ClaimModal = () => {
     const handleClaimTask = async () => {
       UpdateBalanceByTask()
       taskClaimed()
+      updateBalance()
       handleUpdatedBalance()
       handleUpdateBoard()
     };
