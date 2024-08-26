1c8fa86258cdf9ca86eed0cc7c7bc497aaea461a
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-26 03:39:19 +0100

    gsfhsd

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -661,7 +661,7 @@ export const Home2 = () => {
             </div>
             {isFirst && (
                  <div className="inset-0 fixed bg-black/5 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
-                 <div className="w-[100%] py-4 px-4 bg-black/95 rounded-t-2xl h-auto mt-[40px]">
+                 <div className="w-[100%] py-4 px-4 bg-black/95 rounded-t-2xl h-auto mt-[180px]">
                    <div className="">
                    
                    </div>
